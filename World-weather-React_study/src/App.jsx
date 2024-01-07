54e39a9ed784129004caf89bae9c30a33a3b1999
thelight0804 <thelight0804@gmail.com>
2024-01-07 22:03:26 +0900

    Add: GDSC React 스터디 9주차 진행

--- World-weather-React_study/src/App.jsx
+++ World-weather-React_study/src/App.jsx
@@ -1,18 +1,45 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 import Button from 'react-bootstrap/Button';
 
 function App() {
-  const cities = ['서울', '도쿄', '베이징', 'LA', '케나다', '캔버라']; //여기에 값이 1억개가 있어요
+  const cities = ['서울', '도쿄', '베이징', 'LA', '케나다', '캔버라'];
+
+  const [city, setCity] = useState(cities[0]);
+  const [tempature, setTempature] = useState(0);
+  const [weather, setWeather] = useState('맑음');
+
+  const [selected, setSelected] = useState(cities[0]);
 
   return (
     <div>
-      <Button variant="primary" className='city-button'>{cities[0]}</Button>
-      <Button variant="primary" className='city-button'>{cities[1]}</Button>
-      <Button variant="primary" className='city-button'>{cities[2]}</Button>
-      <Button variant="primary" className='city-button'>{cities[3]}</Button>
-      <Button variant="primary" className='city-button'>{cities[4]}</Button>
-      <Button variant="primary" className='city-button'>{cities[5]}</Button>
+      <h1 className='title'>세계 현재 날씨</h1>
+
+      <div className='weather-box'>
+        <div className='weather-info'>
+          <div className='city'>{city}</div>
+          <div>
+            <div className='weather'>{weather}</div>
+            <div className='tempature'>{tempature} °C</div>
+          </div>
+        </div>
+      </div>
+
+      <div className='city-box'>
+        {cities.map((city, index) => {
+          return(
+            <Button 
+              variant='primary'
+              key={index}
+              className={`city-button ${city === selected ? 'city-button-selected' : ''}`}
+              onClick={() => {
+                setCity(cities[index]);
+                setSelected(cities[index]);
+              }}
+            >{city}</Button>
+          )
+        })}
+      </div>
     </div>
   )
 }
