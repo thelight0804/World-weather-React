0b20c77e50d58518f7166fe18919352b9ca2f5bc
thelight0804 <thelight0804@gmail.com>
2024-01-23 15:23:51 +0900

    Add: Japanese page

--- src/AppJP.jsx
+++ src/AppJP.jsx
@@ -4,50 +4,50 @@ import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-function App() {
-  const cities = ['서울', '도쿄', '베이징', 'LA', '케나다', '캔버라', '델리', '런던', '로마', '베를린', '멕시코', '브라질리아', '오사카'];
+function AppJP() {
+  const cities = ['東京', '大阪', 'ソウル', '北京', 'LA', 'カナダ', 'キャンベラ', 'デリー', 'ロンドン', 'ローマ', 'ベルリン', 'メキシコ', 'ブラジリア'];
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
   const [city, setCity] = useState(cities[0]);
   const [engCity, setEngCity] = useState(cityToEnglish(city));
   const [tempature, setTempature] = useState(0);
-  const [weather, setWeather] = useState('맑음');
+  const [weather, setWeather] = useState('晴天');
 
   const [selected, setSelected] = useState(cities[0]);
 
   function cityToEnglish(city) {
     switch (city) {
-      case '서울':
+      case 'ソウル':
         return 'Seoul';
-      case '도쿄':
+      case '東京':
         return 'Tokyo'
-      case '베이징':
+      case '北京':
         return 'Beijing'
       case 'LA':
         return 'Los Angeles'
-      case '케나다':
+      case 'カナダ':
         return 'Canada'
-      case '캔버라':
+      case 'キャンベラ':
         return 'Canberra'
-      case '델리':
+      case 'デリー':
         return 'Delhi'
-      case '런던':
+      case 'ロンドン':
         return 'London'
-      case '로마':
+      case 'ローマ':
         return 'Rome'
-      case '베를린':
+      case 'ベルリン':
         return 'Berlin'
-      case '멕시코':
+      case 'メキシコ':
         return 'Mexico City'
-      case '브라질리아':
+      case 'ブラジリア':
         return 'Brasilia'
-      case '오사카':
+      case '大阪':
         return 'Osaka'
     }
   }
 
   useEffect(() => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${engCity}&appid=${API_KEY}&units=metric&lang=kr`)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${engCity}&appid=${API_KEY}&units=metric&lang=ja`)
       .then((response) => {
         setTempature(response.data.main.temp);
         setWeather(response.data.weather[0].description);
@@ -60,10 +60,10 @@ function App() {
   return (
     <div>
       <div className='lenguage-box'>
-        <Link to='/' className='lenguage__btn'>KR</Link>
-        <Link to='/ja' className='lenguage__btn link-unselected'>JP</Link>
+        <Link to='/' className='lenguage__btn link-unselected'>KR</Link>
+        <Link to='/ja' className='lenguage__btn'>JP</Link>
       </div>
-      <h1 className='title'>세계 현재 날씨</h1>
+      <h1 className='title'>世界の現在の天気</h1>
 
       <div className='weather-box'>
         <div className='weather-info'>
@@ -95,4 +95,4 @@ function App() {
   )
 }
 
-export default App
+export default AppJP
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AppJP from './AppJP.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/ja", element: <AppJP /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
